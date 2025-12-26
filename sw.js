e57700532e58bb108a2b3450fa0ca3d92412d40d
vladosik24 <kuriliak.official@gmail.com>
2025-12-26 14:29:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,14 @@
-self.addEventListener('install', e=>{self.skipWaiting();});
-self.addEventListener('activate', e=>{clients.claim();});
-self.addEventListener('fetch', e=>{e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
+const CACHE_NAME = 'monopoly-ukraine-v1.0.0';
+const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
+
+self.addEventListener('install', e=>{
+    e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(ASSETS)));
+});
+
+self.addEventListener('activate', e=>{
+    e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
+});
+
+self.addEventListener('fetch', e=>{
+    e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+});
